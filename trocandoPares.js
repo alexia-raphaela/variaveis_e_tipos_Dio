72121861d98103b606d79412bf27fc009cdbe26c
Alexia <alexiaraphaela@gmail.com>
2022-05-30 15:29:18 -0300

    utiliza operadores matemáticos e métodos pa

---
+++ trocandoPares.js
@@ -0,0 +1,16 @@
+function elementosPares(array) {
+    if(array.length === 0) {
+        return -1
+    }
+    for(let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0 && array[i] != 0) {
+            array[i] = 0
+        } 
+    }
+    return array
+
+}
+console.log(elementosPares([1, 3, 4, 6, 80, 33, 23, 90]))
+    
+
+
